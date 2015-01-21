f6e9e4661deabe483eb52c765ea17317b76826d7
Ben Ferris <ben.ferris1@gmail.com>
2015-01-21 13:08:48 -0500

    fixed connect page

--- html/scheduling/mongo-connect.php
+++ html/scheduling/mongo-connect.php
@@ -1,7 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-$connection = new MongoClient('mongodb://ben:moufxz@ds031681.mongolab.com:31681/heroku_app33381743;');
-$collection = $connection->resolutions;
-$doc = array('I am testing mongodb'=>true);
-$collection->insert($doc);
+$connection = new MongoClient('mongodb://ben:moufxz@ds031681.mongolab.com:31681/heroku_app33381743');
+$db = $connection->selectDB('heroku_app33381743');
+//$collection = $connection->resolutions;
+//$doc = array('I am testing mongodb'=>true);
+//$collection->insert($doc);
