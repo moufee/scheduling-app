d0d7785173e1c9f408dc325965a313554b47dafc
Ben Ferris <ben.ferris1@gmail.com>
2015-01-21 14:57:19 -0500

    test retrieving data from mongo

--- html/scheduling/mongo-connect.php
+++ html/scheduling/mongo-connect.php
@@ -4,3 +4,12 @@ ini_set("display_errors", 1);
 $connection = new MongoClient('mongodb://ben:moufxz@ds031681.mongolab.com:31681/heroku_app33381743');
 $db = $connection->selectDB('heroku_app33381743');
 $collection = $db->resolutions;
+
+$resolutions = array();
+$cursor = $collection->find();
+foreach ( $cursor as $id => $value )
+{
+    array_push($resolutions,(object)$value);
+}
+
+var_dump($resolutions);
