f1d0c8e146c652d46889c84b4c172040bcfc437f
Ben Ferris <ben.ferris1@gmail.com>
2015-01-28 22:16:46 -0500

    add full stop at end of failure message

--- html/respond.php
+++ html/respond.php
@@ -80,7 +80,7 @@ if($resolution){
                         }
                         if($numberRespondingNo==$numberOfPeople) {
                             //this message goes to Kris
-                            sendPlainMessage('benferris2@gmail.com', 'Problem Resolving Scheduling Conflict', '<p style="font-size:16px;font-family:Arial;">All contacted people have responded "no" to '.$resolution['requester']['name'].'\'s request to fill '.$resolution['position'].' on '.$resolution['weekendDate'],'</p>');
+                            sendPlainMessage('benferris2@gmail.com', 'Problem Resolving Scheduling Conflict', '<p style="font-size:16px;font-family:Arial;">All contacted people have responded "no" to '.$resolution['requester']['name'].'\'s request to fill '.$resolution['position'].' on '.$resolution['weekendDate'],'.</p>');
                         }
                     }
                 }
